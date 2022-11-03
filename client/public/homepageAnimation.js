b97194470112c467eddc5f7a112e7956ea1943b7
ArunKumarSehrawat <arunksehrawat@gmail.com>
2022-11-04 02:55:54 +0530

    Simplified the animation

--- client/public/homepageAnimation.js
+++ client/public/homepageAnimation.js
@@ -1,72 +1,69 @@
 const dots = [];
-const MAX_DOTS = 50;
+let MAX_DOTS = 0;
 const MAX_DISTANCE = 150;
+const TARGET_DENSITY = 0.00018;
 
 function setup() {
-     createCanvas(windowWidth, windowHeight);
-     stroke(255);
-
-     for (let i = 0; i < MAX_DOTS; i++) {
-          if (i === 0) dots.push(new Dot(true));
-          else dots.push(new Dot());
-     }
+     createCanvas(windowWidth, windowHeight, P2D);
+     MAX_DOTS = floor(width * height * TARGET_DENSITY);
+     // create dots
+     for (let i = 0; i < MAX_DOTS; i++) dots.push(new Dot());
 }
 
 function draw() {
      background(18, 22, 28);
-
-     for (const dot of dots) {
-          dot.update();
-          dot.show(dots);
+     for (let i = 0; i < dots.length; i++) {
+          dots[i].show();
+          dots[i].update();
      }
 }
 
 function windowResized() {
      resizeCanvas(windowWidth, windowHeight);
+     let NEW_MAX_DOTS = floor(width * height * TARGET_DENSITY);
+     const difference = NEW_MAX_DOTS - MAX_DOTS;
+     if (difference < 0) dots.splice(0, -difference);
+     else {
+          for (let i = 0; i < difference; i++) dots.push(new Dot());
+     }
+     MAX_DOTS = NEW_MAX_DOTS;
 }
 
-// Dot class
 class Dot {
-     constructor(userControlled = false) {
-          const maxSpeed = 2;
-          this.userControlled = userControlled;
-          this.pos = userControlled ? createVector(mouseX, mouseY) : createVector(random(width), random(height));
-          this.velocity = userControlled ? createVector(0, 0) : createVector(random(-maxSpeed, maxSpeed), random(-maxSpeed, maxSpeed));
-          this.thickness = userControlled ? 5 : random(1, 5);
+     constructor() {
+          this.maxSpeed = 2;
+          this.thickness = random(1, 5);
+          this.pos = { x: random(width), y: random(height) };
+          this.velocity = { x: random(-this.maxSpeed, this.maxSpeed), y: random(-this.maxSpeed, this.maxSpeed) };
+          const variableSpeed = this.maxSpeed / 10;
+          this.randomness = { x: random(-variableSpeed, variableSpeed), y: random(-variableSpeed, variableSpeed) };
      }
 
-     show(dots) {
-          stroke(255);
+     show() {
+          stroke(31, 163, 108);
           strokeWeight(this.thickness);
           point(this.pos.x, this.pos.y);
-
-          for (const dot of dots) {
-               const distance = dist(this.pos.x, this.pos.y, dot.pos.x, dot.pos.y);
-
-               // if distance is less than MAX_DISTANCE, connect them through a line.
-               if (distance <= MAX_DISTANCE) {
-                    const opacity = map(distance, MAX_DISTANCE, 0, 40, 255);
-                    strokeWeight(0.5);
-                    stroke(opacity);
-                    line(this.pos.x, this.pos.y, dot.pos.x, dot.pos.y);
-               }
-          }
      }
 
      update() {
-          // if the dot is controlled by the user, just change it's X and Y.
-          if (this.userControlled) {
-               this.pos.x = mouseX;
-               this.pos.y = mouseY;
-               return;
-          }
-
           // offscreen
-          if (this.pos.x > width) this.pos = createVector(0, this.pos.y);
-          else if (this.pos.x < 0) this.pos = createVector(width, this.pos.y);
-          else if (this.pos.y > height) this.pos = createVector(this.pos.x, 0);
-          else if (this.pos.y < 0) this.pos = createVector(this.pos.x, height);
+          if (this.pos.x > width) this.pos.x = 0;
+          else if (this.pos.x < 0) this.pos.x = width;
+          else if (this.pos.y > height) this.pos.y = this.pos.x;
+          else if (this.pos.y < 0) this.pos.y = height;
 
-          this.pos.add(this.velocity);
+          // update position
+          this.pos.x += this.velocity.x + this.randomness.x;
+          this.pos.y += this.velocity.y + this.randomness.y;
+
+          // parallax effect
+          this.pos.x += map(movedX, 0, width, 0, 100);
+          this.pos.y += map(movedY, 0, height, 0, 100);
+
+          if (frameCount % 100 === 0) {
+               const variableSpeed = this.maxSpeed / 5;
+               this.randomness.x = random(-variableSpeed, variableSpeed);
+               this.randomness.y = random(-variableSpeed, variableSpeed);
+          }
      }
 }
