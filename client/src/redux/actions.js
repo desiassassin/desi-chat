b23e1b079642451ca455b7938939e5ea51340832
ArunKumarSehrawat <arunksehrawat@gmail.com>
2022-11-13 02:31:51 +0530

    Requests sent / received reducers and actions

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -2,3 +2,10 @@ export const USER = {
      LOGGED_IN: "loggedIn",
      LOGGED_OUT: "loggedOut",
 };
+
+export const FRIENDS = {
+     REQUEST_SENT: "requestSent",
+     REQUEST_RECEIVED: "requestReceived",
+     REQUEST_ACCEPTED: "requestAccepted",
+     REQUEST_REJECTED: "requestRejected",
+};
--- client/src/redux/reducers.js
+++ client/src/redux/reducers.js
@@ -10,6 +10,10 @@ export const user = (state = initialState.user, { type, payload }) => {
                return { ...payload };
           case ACTIONS.USER.LOGGED_OUT:
                return {};
+          case ACTIONS.FRIENDS.REQUEST_SENT:
+               return { ...state, friendRequestsSent: [...state.friendRequestsSent, payload] };
+          case ACTIONS.FRIENDS.REQUEST_RECEIVED:
+               return { ...state, friendRequestsPending: [...state.friendRequestsPending, payload] };
           default:
                return false;
      }
